773c6458c54f6f4f0010fa5b195b8c916cc83163
TatianaArslanova <tashilova@narod.ru>
2018-04-13 11:41:35 +0500

    add contract class for database

--- android2_lesson01/src/java/java/com/example/ama/android2_lesson01/db/NotesDatabaseContract.java
+++ android2_lesson01/src/java/java/com/example/ama/android2_lesson01/db/NotesDatabaseContract.java
@@ -6,7 +6,17 @@ public final class NotesDatabaseContract {
     public NotesDatabaseContract() {
     }
 
-    public static abstract class NotesContract implements BaseColumns {
+    public static abstract class NotesTable implements BaseColumns {
+        public static final String TABLE_NAME = "textnotes";
+        public static final String COLOMN_NAME_TITILE = "title";
+        public static final String COLOMN_NAME_TEXT = "text";
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE" + TABLE_NAME + "{" +
+                        _ID + "INTEGER PRIMARY_KEY AUTOINCREMENT," +
+                        COLOMN_NAME_TITILE + "TEXT," +
+                        COLOMN_NAME_TEXT + "TEXT)";
+        public static final String SQL_DROP_TABLE =
+                "DROP TABLE IF EXISTS " + TABLE_NAME;
 
     }
 
--- android2_lesson01/src/java/java/com/example/ama/android2_lesson01/db/NotesDbHelper.java
+++ android2_lesson01/src/java/java/com/example/ama/android2_lesson01/db/NotesDbHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.example.ama.android2_lesson01.db.NotesDatabaseContract.*;
+
 public class NotesDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "notes.db";
     public static final int DATABASE_VERSION = 1;
@@ -14,11 +16,12 @@ public class NotesDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(NotesTable.SQL_CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(NotesTable.SQL_DROP_TABLE);
+        this.onCreate(db);
     }
 }
