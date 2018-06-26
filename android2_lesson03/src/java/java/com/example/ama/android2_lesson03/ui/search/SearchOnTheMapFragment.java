55be332a5b54c1a8e6407658231d7fbb110cde7b
TatianaArslanova <tashilova@narod.ru>
2018-06-26 19:28:42 +0500

    add base map fragment

--- android2_lesson03/src/java/java/com/example/ama/android2_lesson03/ui/search/SearchOnTheMapFragment.java
+++ android2_lesson03/src/java/java/com/example/ama/android2_lesson03/ui/search/SearchOnTheMapFragment.java
@@ -5,22 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ama.android2_lesson03.PocketMap;
 import com.example.ama.android2_lesson03.R;
 import com.example.ama.android2_lesson03.ui.Launcher;
-import com.example.ama.android2_lesson03.ui.MainActivity;
+import com.example.ama.android2_lesson03.ui.search.base.BaseMapFragment;
 import com.example.ama.android2_lesson03.ui.search.base.Controller;
 import com.example.ama.android2_lesson03.ui.search.base.SearchOnTheMapView;
 import com.example.ama.android2_lesson03.ui.search.base.SearchPresenter;
@@ -28,7 +20,6 @@ import com.example.ama.android2_lesson03.ui.search.mvp.SearchOnTheMapPresenter;
 import com.example.ama.android2_lesson03.utils.DialogLauncher;
 import com.example.ama.android2_lesson03.utils.PermissionManager;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -36,29 +27,13 @@ import com.google.android.gms.maps.model.Marker;
 /**
  * Fragment for getting search user queries and showing on the map
  */
-public class SearchOnTheMapFragment extends Fragment implements SearchOnTheMapView {
-
-    private EditText etSearch;
-    private TextView tvAddress;
-    private MapView mapView;
+public class SearchOnTheMapFragment extends BaseMapFragment implements SearchOnTheMapView {
 
     private SearchPresenter<SearchOnTheMapView> presenter;
     private Controller mapController;
 
     public static SearchOnTheMapFragment newInstance() {
         return new SearchOnTheMapFragment();
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
-        super.onCreate(savedInstanceState);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
     @Override
@@ -76,11 +51,9 @@ public class SearchOnTheMapFragment extends Fragment implements SearchOnTheMapVi
                 tvAddress.setText("");
             }
         });
-        initUI(view);
         addListeners(view);
-        mapView.onCreate(savedInstanceState);
-        getMap();
         super.onViewCreated(view, savedInstanceState);
+        getMap();
     }
 
     private void getMap() {
@@ -96,21 +69,18 @@ public class SearchOnTheMapFragment extends Fragment implements SearchOnTheMapVi
     @Override
     public void onStart() {
         presenter.attachView(this);
-        mapView.onStart();
         super.onStart();
     }
 
     @Override
     public void onResume() {
         presenter.subscribeOnLocationUpdates();
-        mapView.onResume();
         super.onResume();
     }
 
     @Override
     public void onPause() {
         presenter.unsubscribeOfLocationUpdates();
-        mapView.onPause();
         super.onPause();
     }
 
@@ -118,43 +88,7 @@ public class SearchOnTheMapFragment extends Fragment implements SearchOnTheMapVi
     public void onStop() {
         mapController.saveState();
         presenter.detachView();
-        mapView.onStop();
         super.onStop();
-    }
-
-    @Override
-    public void onDestroy() {
-        mapView.onDestroy();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        mapView.onSaveInstanceState(outState);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory() {
-        mapView.onLowMemory();
-        super.onLowMemory();
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.search_fragment_menu, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.mi_showsettings:
-                Launcher.runMarkerListFragment((MainActivity) getActivity(), true);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     @Override
@@ -179,9 +113,7 @@ public class SearchOnTheMapFragment extends Fragment implements SearchOnTheMapVi
 
     @Override
     public void showOnGMapsApp(Uri uri) {
-        if (getActivity() != null) {
-            Launcher.sendGoogleMapsIntent((AppCompatActivity) getActivity(), uri);
-        }
+        Launcher.sendGoogleMapsIntent((AppCompatActivity) getActivity(), uri);
     }
 
     @Override
@@ -214,12 +146,6 @@ public class SearchOnTheMapFragment extends Fragment implements SearchOnTheMapVi
                 presenter.saveMarker(marker, inputText);
             }
         });
-    }
-
-    private void initUI(View view) {
-        etSearch = view.findViewById(R.id.et_search);
-        tvAddress = view.findViewById(R.id.tv_address);
-        mapView = view.findViewById(R.id.mv_main_map);
     }
 
     private void addListeners(View view) {
---
+++ android2_lesson03/src/java/java/com/example/ama/android2_lesson03/ui/search/base/BaseMapFragment.java
@@ -0,0 +1,109 @@
+package com.example.ama.android2_lesson03.ui.search.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.ama.android2_lesson03.R;
+import com.example.ama.android2_lesson03.ui.Launcher;
+import com.example.ama.android2_lesson03.ui.MainActivity;
+import com.google.android.gms.maps.MapView;
+
+public abstract class BaseMapFragment extends Fragment {
+
+    protected MapView mapView;
+    protected EditText etSearch;
+    protected TextView tvAddress;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        initUI(view);
+        mapView.onCreate(savedInstanceState);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        mapView.onStart();
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        mapView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        mapView.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        mapView.onLowMemory();
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_fragment_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mi_showsettings:
+                Launcher.runMarkerListFragment((MainActivity) getActivity(), true);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void initUI(View view) {
+        etSearch = view.findViewById(R.id.et_search);
+        tvAddress = view.findViewById(R.id.tv_address);
+        mapView = view.findViewById(R.id.mv_main_map);
+    }
+}
