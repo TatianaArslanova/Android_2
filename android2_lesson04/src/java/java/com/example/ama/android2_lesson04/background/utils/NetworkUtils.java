c6ec8f7996ee85f0bd72919fdfcf7815f2cbcafe
TatianaArslanova <tashilova@narod.ru>
2018-09-08 15:53:47 +0500

    add check internet connection

--- android2_lesson04/src/java/java/com/example/ama/android2_lesson04/background/utils/NetworkUtils.java
+++ android2_lesson04/src/java/java/com/example/ama/android2_lesson04/background/utils/NetworkUtils.java
@@ -1,8 +1,12 @@
 package com.example.ama.android2_lesson04.background.utils;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
+import com.example.ama.android2_lesson04.ServiceTestApp;
 import com.example.ama.android2_lesson04.background.BackgroundConstants;
 
 import java.io.IOException;
@@ -12,38 +16,51 @@ import java.net.URL;
 
 public class NetworkUtils {
     public static Bitmap loadBitmapFromUrl(String url) {
-        HttpURLConnection connection = null;
-        InputStream inputStream = null;
         Bitmap bitmap = null;
-        try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod(BackgroundConstants.REQUEST_METHOD_GET);
-            connection.connect();
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException(BackgroundConstants.HTTP_ERROR_CODE_MESSAGE
-                        + connection.getResponseCode());
-            }
-            inputStream = connection.getInputStream();
-            if (inputStream != null) {
-                bitmap = BitmapFactory.decodeStream(inputStream);
-            }
-            Thread.sleep(2000);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        if (isNetworkConnected()) {
+            HttpURLConnection connection = null;
+            InputStream inputStream = null;
+            try {
+                connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setDoInput(true);
+                connection.setRequestMethod(BackgroundConstants.REQUEST_METHOD_GET);
+                connection.connect();
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    throw new IOException(BackgroundConstants.HTTP_ERROR_CODE_MESSAGE
+                            + connection.getResponseCode());
                 }
-            }
-            if (connection != null) {
-                connection.disconnect();
+                inputStream = connection.getInputStream();
+                if (inputStream != null) {
+                    bitmap = BitmapFactory.decodeStream(inputStream);
+                }
+                Thread.sleep(2000);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (connection != null) {
+                    connection.disconnect();
+                }
             }
         }
         return bitmap;
+    }
+
+    private static boolean isNetworkConnected() {
+        ConnectivityManager manager = (ConnectivityManager) ServiceTestApp.getInstance()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (manager != null) {
+            networkInfo = manager.getActiveNetworkInfo();
+        }
+        return networkInfo != null && networkInfo.isConnected();
     }
 }
