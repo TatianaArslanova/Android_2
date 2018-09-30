d84182a4dee97214f4175becec7f12ad78abafa0
TatianaArslanova <tashilova@narod.ru>
2018-09-30 15:54:37 +0500

    fix bugs: updating widget location on api >=26, location service handle only one request

--- android2_lesson03/src/java/java/com/example/ama/android2_lesson03/widget/LocationWidgetService.java
+++ android2_lesson03/src/java/java/com/example/ama/android2_lesson03/widget/LocationWidgetService.java
@@ -15,6 +15,7 @@ import android.os.Message;
 import android.os.Process;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.example.ama.android2_lesson03.R;
 import com.example.ama.android2_lesson03.repo.data.base.LocManager;
@@ -26,11 +27,14 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class LocationWidgetService extends Service {
 
+    public static final long WAIT_LOCATION_UPDATE = 5000;
     public static final int NOTIFICATION_ID = 1;
+    public static final int FIRST_START_ID = 1;
     public static final String CHANNEL_ID = "channel_01";
     public static final String CHANNEL_NAME = "find_location_channel";
 
     private LocManager locManager;
+    private LocManager.OnLocationSearchResultCallback listener;
     private WidgetModelFactory factory;
     private HandlerThread thread;
     private Handler handler;
@@ -44,35 +48,17 @@ public class LocationWidgetService extends Service {
         thread.start();
         handler = new Handler(thread.getLooper()) {
             @Override
-            public void handleMessage(final Message msg) {
+            public void handleMessage(Message msg) {
                 if (PermissionManager.checkLocationPermission()) {
-                    requestLocation(msg.what);
+                    requestLocation();
                 } else {
                     sendWidgetUpdates(
                             factory.permissionRequiredModel());
-                    stopSelf(msg.what);
+                    stopSelf();
                 }
             }
         };
-        super.onCreate();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        createNotificationChannel();
-        startForeground(NOTIFICATION_ID, buildNotification());
-        handler.sendMessage(handler.obtainMessage(startId));
-        return START_NOT_STICKY;
-    }
-
-    @Override
-    public void onDestroy() {
-        thread.quit();
-        super.onDestroy();
-    }
-
-    private void requestLocation(final int startId) {
-        locManager.findMyLocation(new LocManager.OnLocationSearchResultCallback() {
+        listener = new LocManager.OnLocationSearchResultCallback() {
             @Override
             public void onLocationFound(Location location) {
                 sendWidgetUpdates(
@@ -82,14 +68,41 @@ public class LocationWidgetService extends Service {
                                         new LatLng(
                                                 location.getLatitude(),
                                                 location.getLongitude()))));
-                stopSelf(startId);
             }
 
             @Override
             public void onError(String message) {
-                stopSelf(startId);
             }
-        });
+        };
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LocationWidgetService.class.getSimpleName(),
+                "STARTED" + String.valueOf(startId));
+        if (startId == FIRST_START_ID) {
+            createNotificationChannel();
+            startForeground(NOTIFICATION_ID, buildNotification());
+            handler.sendEmptyMessage(startId);
+            handler.postDelayed(this::stopWork, WAIT_LOCATION_UPDATE);
+        } else {
+            Log.d(LocationWidgetService.class.getSimpleName(),
+                    "NOT HANDLED" + String.valueOf(startId));
+        }
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        thread.quit();
+        Log.d(LocationWidgetService.class.getSimpleName(), "OnDestroy");
+        super.onDestroy();
+    }
+
+    private void requestLocation() {
+        locManager.findMyLocation(listener);
+        locManager.subscribeOnLocationChanges(listener);
     }
 
     private void sendWidgetUpdates(WidgetModel widgetModel) {
@@ -118,6 +131,11 @@ public class LocationWidgetService extends Service {
         return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
                 .setContentTitle(getString(R.string.notification_message_detecting_location))
                 .build();
+    }
+
+    private void stopWork() {
+        locManager.unsubscribeOfLocationChanges();
+        stopSelf();
     }
 
     @Nullable
