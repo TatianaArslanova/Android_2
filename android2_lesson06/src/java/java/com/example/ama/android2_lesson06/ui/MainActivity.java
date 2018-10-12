e010a177c44c3ef6ce29bd3d84b0eaf96c1c6ef8
TatianaArslanova <tashilova@narod.ru>
2018-10-13 04:04:40 +0500

    add support inserting sms for api >= 19, add fake components

--- android2_lesson06/src/java/java/com/example/ama/android2_lesson06/ui/MainActivity.java
+++ android2_lesson06/src/java/java/com/example/ama/android2_lesson06/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ama.android2_lesson06.ui;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -16,6 +19,8 @@ import com.tbruyelle.rxpermissions2.RxPermissions;
 import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity implements SmsExampleView {
+    private final static int RESULT_REQUEST = 1;
+
     private SmsCursorAdapter adapter;
     private SmsPresenter<SmsExampleView> presenter;
     private Disposable disposable;
@@ -48,15 +53,11 @@ public class MainActivity extends AppCompatActivity implements SmsExampleView {
         super.onStop();
     }
 
-    private void tuneList() {
-        adapter = new SmsCursorAdapter(this);
-        adapter.initLoader();
-        ((ListView) findViewById(R.id.lv_sms_list)).setAdapter(adapter);
-    }
-
-    private void setListeners() {
-        findViewById(R.id.btn_export).setOnClickListener(view -> presenter.exportMessages(adapter.getCursor()));
-        findViewById(R.id.btn_import).setOnClickListener(view -> presenter.importMessages());
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == RESULT_REQUEST && resultCode == RESULT_OK) {
+            if (shouldImport()) presenter.importMessages();
+        }
     }
 
     @Override
@@ -71,5 +72,31 @@ public class MainActivity extends AppCompatActivity implements SmsExampleView {
     @Override
     public void showMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void tuneList() {
+        adapter = new SmsCursorAdapter(this);
+        adapter.initLoader();
+        ((ListView) findViewById(R.id.lv_sms_list)).setAdapter(adapter);
+    }
+
+    private void setListeners() {
+        findViewById(R.id.btn_export).setOnClickListener(view -> presenter.exportMessages(adapter.getCursor()));
+        findViewById(R.id.btn_import).setOnClickListener(view -> {
+            if (shouldImport()) presenter.importMessages();
+            else changeDefaultRequest();
+        });
+    }
+
+    private boolean shouldImport() {
+        return android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.KITKAT ||
+                Telephony.Sms.getDefaultSmsPackage(getApplicationContext()).equals(getPackageName());
+    }
+
+    private void changeDefaultRequest() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+            startActivityForResult(intent, RESULT_REQUEST);
+        }
     }
 }
