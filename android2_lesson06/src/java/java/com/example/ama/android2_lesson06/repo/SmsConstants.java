e0991b66e56264888858e0d8654850d4805957d7
TatianaArslanova <tashilova@narod.ru>
2018-10-13 04:19:31 +0500

    refactoring

---
+++ android2_lesson06/src/java/java/com/example/ama/android2_lesson06/repo/SmsConstants.java
@@ -0,0 +1,6 @@
+package com.example.ama.android2_lesson06.repo;
+
+public class SmsConstants {
+    public static final String ADDRESS = "address";
+    public static final String BODY = "body";
+}
--- android2_lesson06/src/java/java/com/example/ama/android2_lesson06/ui/adapter/SmsCursorAdapter.java
+++ android2_lesson06/src/java/java/com/example/ama/android2_lesson06/ui/adapter/SmsCursorAdapter.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.ama.android2_lesson06.repo.SmsCursorLoader;
+import com.example.ama.android2_lesson06.repo.SmsConstants;
 
 public class SmsCursorAdapter extends SimpleCursorAdapter {
     private final Uri URI = Uri.parse("content://sms");
@@ -19,7 +20,7 @@ public class SmsCursorAdapter extends SimpleCursorAdapter {
 
     public SmsCursorAdapter(Context context) {
         super(context, android.R.layout.simple_list_item_2, null,
-                new String[]{"address", "body"},
+                new String[]{SmsConstants.ADDRESS, SmsConstants.BODY},
                 new int[]{android.R.id.text1, android.R.id.text2}, 0);
     }
 
@@ -33,7 +34,7 @@ public class SmsCursorAdapter extends SimpleCursorAdapter {
         @NonNull
         @Override
         public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
-            return new SmsCursorLoader(mContext, URI, null, null, null, null);
+            return new CursorLoader(mContext, URI, null, null, null, null);
         }
 
         @Override
