a0475cd7267bf778a5447797d2e3ed566e06e2da
TatianaArslanova <tashilova@narod.ru>
2018-10-29 17:21:57 +0500

    add simple animation

--- lesson07/src/java/java/com/example/ama/lesson07/MainActivity.java
+++ lesson07/src/java/java/com/example/ama/lesson07/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.ama.lesson07;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.sv_circle).setOnClickListener(view -> view.startAnimation(
+                AnimationUtils.loadAnimation(MainActivity.this, R.anim.simple_animation)));
+        findViewById(R.id.sv_square).setOnClickListener(view -> view.startAnimation(
+                AnimationUtils.loadAnimation(MainActivity.this, R.anim.simple_animation)));
+        findViewById(R.id.sv_triangle).setOnClickListener(view -> view.startAnimation(
+                AnimationUtils.loadAnimation(MainActivity.this, R.anim.simple_animation)));
     }
 }
