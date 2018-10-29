d12e53793af5dc2466d66b6be97ebd0e1f5e90c8
TatianaArslanova <tashilova@narod.ru>
2018-10-29 17:20:40 +0500

    add ShapeView

---
+++ lesson07/src/java/java/com/example/ama/lesson07/ShapeView.java
@@ -0,0 +1,82 @@
+package com.example.ama.lesson07;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ShapeView extends View {
+    private static final int DEFAULT_CORNERS = 0;
+    private Paint shapePaint;
+    private int corners;
+
+    public ShapeView(Context context) {
+        super(context);
+    }
+
+    public ShapeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ShapeView);
+        corners = array.getInteger(R.styleable.ShapeView_shapeType, DEFAULT_CORNERS);
+        int color = array.getColor(R.styleable.ShapeView_shapeColor, Color.GRAY);
+        shapePaint = new Paint();
+        shapePaint.setColor(color);
+        array.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        switch (corners) {
+            case 0:
+                drawCircle(canvas);
+                break;
+            case 3:
+                drawTriangle(canvas);
+                break;
+            case 4:
+                drawSquare(canvas);
+                break;
+        }
+    }
+
+    private void drawTriangle(Canvas canvas) {
+        int canWidth = canvas.getWidth();
+        int canHeight = canvas.getHeight();
+        boolean widthLess = canWidth < canHeight;
+        int leftCornerX = widthLess ? 0 : canWidth / 2 - canHeight / 2;
+        int bottomY = widthLess ? canHeight / 2 + canWidth / 2 : canHeight;
+        int rightCornerX = widthLess ? canWidth : canWidth / 2 + canHeight / 2;
+        int topCornerX = canWidth / 2;
+        int topY = widthLess ? canHeight / 2 - canWidth / 2 : 0;
+        Path path = new Path();
+        path.moveTo(leftCornerX, bottomY);
+        path.lineTo(rightCornerX, bottomY);
+        path.lineTo(topCornerX, topY);
+        path.lineTo(leftCornerX, bottomY);
+        canvas.drawPath(path, shapePaint);
+    }
+
+    private void drawCircle(Canvas canvas) {
+        int centerX = canvas.getWidth() / 2;
+        int centerY = canvas.getHeight() / 2;
+        int radius = canvas.getWidth() <= canvas.getHeight() ?
+                canvas.getWidth() / 2 : canvas.getHeight() / 2;
+        canvas.drawCircle(centerX, centerY, radius, shapePaint);
+    }
+
+    private void drawSquare(Canvas canvas) {
+        int canWidth = canvas.getWidth();
+        int canHeight = canvas.getHeight();
+        boolean widthLess = canWidth < canHeight;
+        int left = widthLess ? 0 : canWidth / 2 - canHeight / 2;
+        int top = widthLess ? canHeight / 2 - canWidth / 2 : 0;
+        int right = widthLess ? canWidth : canWidth / 2 + canHeight / 2;
+        int bottom = widthLess ? canHeight / 2 + canWidth / 2 : canHeight;
+        canvas.drawRect(left, top, right, bottom, shapePaint);
+    }
+}
