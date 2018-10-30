c84973a3a573dbfd04b472ea848e751ea0747a1b
TatianaArslanova <tashilova@narod.ru>
2018-10-30 21:02:07 +0500

    add ShapeView (kotlin)

--- lesson07/src/kotlin/java/com/example/ama/lesson07/MainActivity.kt
+++ lesson07/src/kotlin/java/com/example/ama/lesson07/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.ama.lesson07
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sv_circle.setOnClickListener {
+            it.startAnimation(AnimationUtils.loadAnimation(this, R.anim.simple_animation))
+        }
+        sv_square.setOnClickListener {
+            val set = AnimatorInflater.loadAnimator(this, R.animator.rotation_animator) as AnimatorSet
+            set.setTarget(it)
+            set.start()
+        }
+        sv_triangle.setOnClickListener {
+            val set = AnimatorInflater.loadAnimator(this, R.animator.jump_animation) as AnimatorSet
+            set.setTarget(it)
+            set.start()
+        }
     }
 }
---
+++ lesson07/src/kotlin/java/com/example/ama/lesson07/ShapeView.kt
@@ -0,0 +1,96 @@
+package com.example.ama.lesson07
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+class ShapeView : View {
+
+    companion object {
+        const val DEFAULT_SIZE = 100
+        const val DEFAULT_CORNERS = 0
+    }
+
+    private val shapePaint = Paint()
+    private var corners: Int = 0
+    var shapeColor: Int = Color.GRAY
+        set(value) {
+            field = value
+            shapePaint.setColor(field)
+            invalidate()
+        }
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        val array = context.obtainStyledAttributes(attributeSet, R.styleable.ShapeView)
+        corners = array.getInteger(R.styleable.ShapeView_shapeType, DEFAULT_CORNERS)
+        shapeColor = array.getColor(R.styleable.ShapeView_shapeColor, Color.GRAY)
+        shapePaint.color = shapeColor
+        array.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        if (canvas != null) {
+            when (corners) {
+                0 -> drawCircle(canvas)
+                3 -> drawTriangle(canvas)
+                4 -> drawSquare(canvas)
+            }
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(
+                measureSize(widthMeasureSpec, paddingLeft + paddingRight),
+                measureSize(heightMeasureSpec, paddingBottom + paddingTop)
+        )
+    }
+
+    private fun drawCircle(canvas: Canvas) {
+        val centerX = canvas.width / 2
+        val centerY = canvas.height / 2
+        val radius = if (canvas.width <= canvas.height) canvas.width / 2 else canvas.height / 2
+        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), radius.toFloat(), shapePaint)
+    }
+
+    private fun drawTriangle(canvas: Canvas) {
+        val widthLess = canvas.width < canvas.height
+        val leftCornerX = if (widthLess) 0 else canvas.width / 2 - canvas.height / 2
+        val bottomY = if (widthLess) canvas.height / 2 + canvas.width / 2 else canvas.height
+        val rightCornerX = if (widthLess) canvas.width else canvas.width / 2 + canvas.height / 2
+        val topCornerX = canvas.width / 2
+        val topY = if (widthLess) canvas.height / 2 - canvas.width / 2 else 0
+        val path = Path()
+        path.moveTo(leftCornerX.toFloat(), bottomY.toFloat())
+        path.lineTo(rightCornerX.toFloat(), bottomY.toFloat())
+        path.lineTo(topCornerX.toFloat(), topY.toFloat())
+        path.lineTo(leftCornerX.toFloat(), bottomY.toFloat())
+        canvas.drawPath(path, shapePaint)
+    }
+
+    private fun drawSquare(canvas: Canvas) {
+        val widthLess = canvas.width < canvas.height
+        val left = if (widthLess) 0 else canvas.width / 2 - canvas.height / 2
+        val top = if (widthLess) canvas.height / 2 - canvas.width / 2 else 0
+        val right = if (widthLess) canvas.width else canvas.width / 2 + canvas.height / 2
+        val bottom = if (widthLess) canvas.height / 2 + canvas.width / 2 else canvas.height
+        canvas.drawRect(left.toFloat(), top.toFloat(), right.toFloat(), bottom.toFloat(), shapePaint)
+    }
+
+    private fun measureSize(measureSpec: Int, paddingSum: Int): Int {
+        val mode = View.MeasureSpec.getMode(measureSpec)
+        val size = View.MeasureSpec.getSize(measureSpec)
+        return if (mode == MeasureSpec.EXACTLY) {
+            size
+        } else {
+            if (mode == MeasureSpec.AT_MOST) {
+                Math.min(DEFAULT_SIZE + paddingSum, size)
+            } else DEFAULT_SIZE + paddingSum
+        }
+    }
+}
